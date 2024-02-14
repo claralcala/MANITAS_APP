a6524733dad4d5877f2505936cf7ab1f00f50438
Clara Alcalá García <calcgar312@iescarrillo.es>
2024-02-14 12:56:27 +0100

    tokens

--- src/main/java/com/app/maniManitas/security/SecurityConfig.java
+++ src/main/java/com/app/maniManitas/security/SecurityConfig.java
@@ -2,16 +2,45 @@ package com.app.maniManitas.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	
+	//Este metodo va a encargarse de verificar la informacion de los usuarios que se loguean en la API
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 
+	//Para encriptar la contraseña del usuario
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	//Establece una cadena de filtros en la API, además determina los roles de los usuarios para acceder a ciertas url
+	@Bean
+	SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+		
+		http
+			.csrf().disable()
+			.authorizeHttpRequests() //Peticiones http deben estar permitidas
+			.requestMatchers("/usuario/**").permitAll() //todas las url de usuario
+			.requestMatchers(HttpMethod.POST, "/category/**").hasAnyAuthority("ADMIN")
+			.and()
+			.httpBasic();
+		
+		return http.build();
 	}
 }
--- src/main/java/com/app/maniManitas/service/UsuarioService.java
+++ src/main/java/com/app/maniManitas/service/UsuarioService.java
@@ -1,9 +1,16 @@
 package com.app.maniManitas.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.app.maniManitas.entity.Category;
@@ -12,7 +19,7 @@ import com.app.maniManitas.repository.CategoryRepository;
 import com.app.maniManitas.repository.UsuarioRepository;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService{
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
@@ -28,6 +35,22 @@ public class UsuarioService {
 	
 	public void save(Usuario u) {
 		this.usuarioRepository.save(u);
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findByUsername(username);
+		
+		if(usuario.isEmpty()) {
+			throw new UsernameNotFoundException("Usuario no encontrado");
+		}
+		
+		Usuario u = usuario.get();
+		List<GrantedAuthority> autoridades = new ArrayList<GrantedAuthority>(); 
+		autoridades.add(new SimpleGrantedAuthority(u.getRol().toString()));
+		
+		return new User(u.getUsername(), u.getPassword(), autoridades);
 	}
 	
 	
